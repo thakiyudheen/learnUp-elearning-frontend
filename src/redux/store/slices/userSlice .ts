f662d7ae1c11ce8e3caa981496fe9a64c1fd4858
thakiyudheen <thakeyudheen525@gmail.com>
2024-07-16 00:47:48 +0530

    profile again update

--- src/redux/store/slices/userSlice .ts
+++ src/redux/store/slices/userSlice .ts
@@ -4,6 +4,7 @@ import { signupAction } from '../actions/auth/signupAction';
 import { getUserDataAction } from '../actions/auth/getUserDataAction';
 import { logoutAction } from '../actions/auth/logoutAction';
 import { loginUserAction } from '../actions/auth/loginUserAction';
+import { updateUserAction } from '../actions/user/updateUserAction';
 
 export interface UserState {
     loading: boolean;
@@ -100,6 +101,24 @@ const userSlice = createSlice({
             state.loading = true ; 
             state.data =null ;
             state.error = action.error.message || 'Login failed';
+        })
+
+        // update data handle -----------------------------------------
+        
+        .addCase(updateUserAction.pending , (state : UserState) =>{
+            console.log('update user is working')
+            state.loading = true;
+            state.error = null;
+        })
+        .addCase(updateUserAction.fulfilled , (state : UserState , action : PayloadAction<SignupFormData>) =>{
+            state.loading = false;
+            state.data = action.payload;
+            state.error = null;
+        })
+        .addCase(updateUserAction.rejected,(state : UserState , action ) =>{
+            state.loading = true ; 
+            state.data =null ;
+            state.error = action.error.message || 'updation failed';
         })
 
     },
